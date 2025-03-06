e1029bdd3a73e6df2bf643df8083b5fe45f0c798
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2025-03-07 03:08:58 +0700

    update

--- files/cfg/config/nvim/lua/plugins/colorschema.lua
+++ files/cfg/config/nvim/lua/plugins/colorschema.lua
@@ -1,40 +1,10 @@
 vim.cmd([[ set background=dark ]])
 vim.g.transparent = true
 
--- mellifluous lackluster
+-- mellifluous kanagawa hybrid
 vim.g.colorscheme = "mellifluous"
 
 return {
-  {
-    "slugbyte/lackluster.nvim",
-    lazy = false,
-    priority = 1000,
-    init = function() end,
-    config = function()
-      local lackluster = require("lackluster")
-      lackluster.setup({
-        tweak_syntax = {
-          string = "default",
-          string_escape = "default",
-          comment = "default",
-          builtin = "default", -- builtin modules and functions
-          type = "default",
-          keyword = "default",
-          keyword_return = "default",
-          keyword_exception = "default",
-        },
-        tweak_background = {
-          normal = "none", -- transparent
-        },
-      })
-
-      if vim.g.colorscheme == "lackluster" then
-        -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
-        -- vim.cmd.colorscheme("lackluster-mint")
-        vim.cmd.colorscheme("lackluster-mint")
-      end
-    end,
-  },
   {
     "ramojus/mellifluous.nvim",
     config = function()
@@ -46,6 +16,27 @@ return {
       })
       if vim.g.colorscheme == "mellifluous" then
         vim.cmd("colorscheme mellifluous")
+      end
+    end,
+  },
+
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      if vim.g.colorscheme == "kanagawa" then
+        vim.cmd("colorscheme kanagawa")
+      end
+    end,
+  },
+
+  {
+    "HoNamDuong/hybrid.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      if vim.g.colorscheme == "hybrid" then
+        vim.cmd("colorscheme hybrid")
       end
     end,
   },

7aae4b6acce7b7552e9b5c7b2f2ef99f5fbcea56
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2025-05-30 07:06:31 +0700

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -243,7 +243,8 @@ in
     material-icons
     symbola
     # typestarFont # Note: Ensure this package exists in your nixpkgs; it may be custom or from a specific source
-    (nerdfonts.override { fonts = [ "JetBrainsMono" "IBMPlexMono" "CascadiaCode" ]; })
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.blex-mono
     fira-code
     hack-font
     source-code-pro

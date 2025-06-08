da046d4f8e78abd13b59b4ce776e4cb3da8e6948
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2025-06-09 01:33:02 +0700

    update

--- files/cfg/config/nvim/lua/plugins/colorschema.lua
+++ files/cfg/config/nvim/lua/plugins/colorschema.lua
@@ -1,8 +1,8 @@
 vim.cmd([[ set background=dark ]])
 vim.g.transparent = true
 
--- mellifluous kanagawa hybrid
-vim.g.colorscheme = "kanagawa"
+-- mellifluous kanagawa hybrid kanagawabones zenwritten
+vim.g.colorscheme = "mellifluous"
 
 return {
   {
@@ -25,6 +25,26 @@ return {
     config = function()
       if vim.g.colorscheme == "kanagawa" then
         vim.cmd("colorscheme kanagawa")
+      end
+    end,
+  },
+
+  {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    config = function()
+      --     vim.g.zenbones_darken_comments = 45
+      if vim.g.colorscheme == "kanagawabones" then
+        vim.cmd.colorscheme("kanagawabones")
+      end
+      if vim.g.colorscheme == "zenwritten" then
+        vim.cmd("colorscheme zenwritten")
       end
     end,
   },
---
+++ files/cfg/config/nvim/lua/plugins/unified.lua
@@ -0,0 +1,10 @@
+return {
+  "axkirillov/unified.nvim",
+  keys = {
+    { "<leader>gu", "<cmd>Unified<cr>", desc = "Unified" },
+    { "<leader>gq", "<cmd>Unified reset<cr>", desc = "Unified reset" },
+  },
+  opts = {
+    -- your configuration comes here
+  },
+}

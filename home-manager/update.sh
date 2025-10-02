c6c535d17c752c10127a39e6a2c4d1d1b65d3d40
Agung Baptiso Sorlawan <agung.b.sorlawan@gdplabs.id>
2025-10-02 14:39:40 +0700

    update.sh: fix empty-arg forwarding when no extra args are given

--- home-manager/update.sh
+++ home-manager/update.sh
@@ -1,3 +1,39 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-home-manager switch --extra-experimental-features 'nix-command flakes' --flake .
+VERBOSE=0
+
+ARGS=()
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -v|--verbose)
+      VERBOSE=1
+      shift
+      ;;
+    -h|--help)
+      echo "Usage: $0 [-v|--verbose] [extra home-manager args...]"
+      echo "Example: $0 -v --no-substitute"
+      exit 0
+      ;;
+    *)
+      ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+HM_ARGS=(
+  --extra-experimental-features "nix-command flakes"
+  --flake .
+)
+
+if [[ $VERBOSE -eq 1 ]]; then
+  set -x
+  HM_ARGS+=(--verbose --show-trace)
+fi
+
+if (( ${#ARGS[@]} > 0 )); then
+  home-manager switch "${HM_ARGS[@]}" "${ARGS[@]}"
+else
+  home-manager switch "${HM_ARGS[@]}"
+fi

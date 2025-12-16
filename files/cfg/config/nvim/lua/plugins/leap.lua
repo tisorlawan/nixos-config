c0734afbca49daf3a6240fc5bce83608f6aaa69f
Agung Baptiso Sorlawan <agung.b.sorlawan@gdplabs.id>
2025-12-16 12:12:21 +0700

    update

--- files/cfg/config/nvim/lua/plugins/leap.lua
+++ files/cfg/config/nvim/lua/plugins/leap.lua
@@ -1,5 +1,5 @@
 return {
-  "ggandor/leap.nvim",
+  "https://codeberg.org/andyg/leap.nvim",
   -- commit = "5ae080b646021bbb6e1d8715b155b1e633e28166",
   keys = {
     { "s", "<Plug>(leap-forward)", mode = { "n", "x", "o" }, desc = "leap forward to" },

6bbe3b33c8dbf7778a174db5d36d43cf7844aa43
Agung Baptiso Sorlawan <agung.b.sorlawan@gdplabs.id>
2025-12-28 20:46:06 +0700

    Update

--- files/cfg/config/fish/lib/01_paths.fish
+++ files/cfg/config/fish/lib/01_paths.fish
@@ -4,4 +4,14 @@ set -x PATH "$HOME/.cargo/bin" $PATH
 set -x PATH "$PATH" "$HOME/.local/share/nvim/mason/bin"
 set -x PATH "$HOME/.emacs.d/bin" $PATH
 set -x PATH "$HOME/.local/share/gem/ruby/3.3.0/bin" $PATH
+
+set -x ANDROID_HOME "$HOME/Android/Sdk"
+set -x PATH "$PATH" "$ANDROID_HOME/emulator"
+set -x PATH "$PATH" "$ANDROID_HOME/platform-tools"
+
+
+set -x JAVA_HOME /usr/lib/jvm/java-17-openjdk-amd64
+set -x PATH $JAVA_HOME/bin $PATH
+
+
 # set -x PATH "$HOME/.cache/npm/global/bin" $PATH

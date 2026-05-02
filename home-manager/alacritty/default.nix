47367bf0d91e0cb4a62e17bbedcb17e9a349ce2f
Agung Baptiso Sorlawan <agung.b.sorlawan@gdplabs.id>
2026-05-02 08:24:39 +0700

    Unset NIX env from shell

--- home-manager/alacritty/default.nix
+++ home-manager/alacritty/default.nix
@@ -8,6 +8,9 @@ pkgs.stdenv.mkDerivation {
 
   installPhase = ''
     mkdir -p $out/bin
+    # Alacritty itself needs Nix GL/Mesa when running on Ubuntu.  The interactive
+    # shell is cleaned separately by ~/.scripts/clean-terminal-shell so dev
+    # shells don't inherit these graphics overrides.
     makeWrapper ${pkgs.alacritty}/bin/alacritty $out/bin/alacritty \
       --set LD_LIBRARY_PATH "${pkgs.libglvnd}/lib:${pkgs.mesa}/lib"
   '';
--- home-manager/ghostty/default.nix
+++ home-manager/ghostty/default.nix
@@ -15,6 +15,9 @@ pkgs.stdenv.mkDerivation {
   buildInputs = with pkgs; [ ghostty-pkg libglvnd mesa ];
   installPhase = ''
     mkdir -p $out/bin
+    # Ghostty itself needs Nix GL/Mesa when running on Ubuntu.  The interactive
+    # shell is cleaned separately by ~/.scripts/clean-terminal-shell so dev
+    # shells don't inherit these graphics overrides.
     makeWrapper ${ghostty-pkg}/bin/ghostty $out/bin/ghostty \
       --set LD_LIBRARY_PATH "${pkgs.libglvnd}/lib:${pkgs.mesa}/lib"
   '';
